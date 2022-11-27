f99a66c948567d100007aa519c441fc471acaa5e
Jasper-Chua <75113066+Jasper-Chua@users.noreply.github.com>
2022-11-27 15:16:36 +0800

    Edit Username and Edit Image

--- app/src/main/java/ph/dlsu/mobdeve/dayon/elijah/s11/mco2/adapter/NotificationItemAdapter.kt
+++ app/src/main/java/ph/dlsu/mobdeve/dayon/elijah/s11/mco2/adapter/NotificationItemAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import ph.dlsu.mobdeve.dayon.elijah.s11.mco2.R
 import ph.dlsu.mobdeve.dayon.elijah.s11.mco2.activities.FrontEndNovelActivity
