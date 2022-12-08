360621ff11affe73ec97e039b1115b14d02432df
Jasper-Chua <75113066+Jasper-Chua@users.noreply.github.com>
2022-12-09 00:07:42 +0800

    Search Result

--- app/src/main/java/ph/dlsu/mobdeve/dayon/elijah/s11/mco2/fragments/SearchResultFragment.kt
+++ app/src/main/java/ph/dlsu/mobdeve/dayon/elijah/s11/mco2/fragments/SearchResultFragment.kt
@@ -1,10 +1,17 @@
 package ph.dlsu.mobdeve.dayon.elijah.s11.mco2.fragments
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
+import ph.dlsu.mobdeve.dayon.elijah.s11.mco2.adapter.NotificationItemAdapter
 import ph.dlsu.mobdeve.dayon.elijah.s11.mco2.adapter.SearchResultNavAdapter
+import ph.dlsu.mobdeve.dayon.elijah.s11.mco2.databinding.FragmentNotificationBinding
 import ph.dlsu.mobdeve.dayon.elijah.s11.mco2.databinding.FragmentSearchResultBinding
 import ph.dlsu.mobdeve.dayon.elijah.s11.mco2.model.Episode
 
@@ -20,49 +27,94 @@ class SearchResultFragment : Fragment() {
     private lateinit var dbEssay: DatabaseReference
     private lateinit var dbNSFW: DatabaseReference
 
+    private val appContext = requireContext().applicationContext
+    private var profileId: String = FirebaseAuth.getInstance().currentUser!!.uid
     private var resultList= arrayListOf<String>()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = FragmentSearchResultBinding.inflate(layoutInflater)
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        binding = FragmentSearchResultBinding.inflate(inflater, container, false)
 
 
 
+        binding.searchResultRV.layoutManager = LinearLayoutManager(appContext)
 
+        fetchFilterFirebase()
 
+        return binding.root
+    }
+
+    override fun onViewCreated(itemView: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(itemView, savedInstanceState)
+        binding.searchResultRV.apply {
+            // set a LinearLayoutManager to handle Android
+            // RecyclerView behavior
+            layoutManager = LinearLayoutManager(activity)
+            // set the custom adapter to the RecyclerView
+            adapter = SearchResultNavAdapter(resultList)
+        }
     }
 
     private fun fetchFilterFirebase(){
-        dbSeries = FirebaseDatabase.getInstance().getReference("Series")
+        lateinit var database: DatabaseReference
+        lateinit var queryFirst: Query
+        lateinit var querySecond: Query
 
-        var querySeriesPopularity = dbSeries.orderByChild("Popularity")
-        var querySeriesRecent = dbSeries.orderByChild("Recent")
-        var querySeriesOngoing = dbSeries.orderByChild("Ongoing")
-        var querySeriesFinished = dbSeries.orderByChild("Finished")
+        var filterPopularity: Boolean = false
+        var filterRecent: Boolean = false
 
-        dbNovel = FirebaseDatabase.getInstance().getReference("Novel")
+        var filterOngoing: Boolean = false
+        var filterFinished: Boolean = false
 
-        var queryNovelPopularity = dbNovel.orderByChild("Popularity")
-        var queryNovelRecent = dbNovel.orderByChild("Recent")
-        var queryNovelOngoing = dbNovel.orderByChild("Ongoing")
-        var queryNovelFinished = dbNovel.orderByChild("Finished")
+        var filterSeries: Boolean = false
+        var filterNovel: Boolean = false
+        var filterEssay: Boolean = false
+        var filterNSFW: Boolean = false
 
-        dbEssay = FirebaseDatabase.getInstance().getReference("Essay")
 
-        var queryEssayPopularity = dbEssay.orderByChild("Popularity")
-        var queryEssayRecent = dbEssay.orderByChild("Recent")
-        var queryEssayOngoing = dbEssay.orderByChild("Ongoing")
-        var queryEssayFinished = dbEssay.orderByChild("Finished")
 
-        dbNSFW = FirebaseDatabase.getInstance().getReference("NSFW")
+        if(filterSeries){
+            database = FirebaseDatabase.getInstance().getReference("Series")
+        }
+        else if(filterNovel){
+            database = FirebaseDatabase.getInstance().getReference("Novel")
+        }
+        else if(filterEssay){
+            database = FirebaseDatabase.getInstance().getReference("Essay")
+        }
+        else if(filterNSFW){
+            database = FirebaseDatabase.getInstance().getReference("NSFW")
+        }
 
-        var queryNSFWPopularity = dbNSFW.orderByChild("Popularity")
-        var queryNSFWRecent = dbNSFW.orderByChild("Recent")
-        var queryNSFWOngoing = dbNSFW.orderByChild("Ongoing")
-        var queryNSFWFinished = dbNSFW.orderByChild("Finished")
+        if(filterPopularity){
+            queryFirst = database.orderByChild("Popularity").equalTo("Popularity")
+        }
+        else if(filterRecent){
+            queryFirst = database.orderByChild("Recent").equalTo("Recent")
+        }
+        else{
+            queryFirst = database
+        }
 
-        //change depends on the button clicked
-        querySeriesPopularity.addValueEventListener(object : ValueEventListener{
+        if(filterOngoing){
+            querySecond = queryFirst.orderByChild("Ongoing").equalTo("Ongoing")
+        }
+        else if(filterFinished){
+            querySecond = queryFirst.orderByChild("Finished").equalTo("Finished")
+        }
+        else{
+            querySecond = queryFirst
+        }
+
+
+
+        querySecond.addValueEventListener(object : ValueEventListener{
             override fun onDataChange(snapshot: DataSnapshot) {
                 resultList.clear()
                 if (snapshot.exists()) {
