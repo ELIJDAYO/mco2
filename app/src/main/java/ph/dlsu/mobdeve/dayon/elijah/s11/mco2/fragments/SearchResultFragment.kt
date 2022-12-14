6e0b26c0af9a9d6f2254f9fcb55f8b52bf384583
Jasper-Chua <75113066+Jasper-Chua@users.noreply.github.com>
2022-12-15 00:24:53 +0800

    Update SearchResultFragment.kt

--- app/src/main/java/ph/dlsu/mobdeve/dayon/elijah/s11/mco2/fragments/SearchResultFragment.kt
+++ app/src/main/java/ph/dlsu/mobdeve/dayon/elijah/s11/mco2/fragments/SearchResultFragment.kt
@@ -1,9 +1,12 @@
 package ph.dlsu.mobdeve.dayon.elijah.s11.mco2.fragments
 
+import android.icu.text.SimpleDateFormat
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -14,6 +17,7 @@ import ph.dlsu.mobdeve.dayon.elijah.s11.mco2.adapter.SearchResultNavAdapter
 import ph.dlsu.mobdeve.dayon.elijah.s11.mco2.databinding.FragmentNotificationBinding
 import ph.dlsu.mobdeve.dayon.elijah.s11.mco2.databinding.FragmentSearchResultBinding
 import ph.dlsu.mobdeve.dayon.elijah.s11.mco2.model.Episode
+import java.util.*
 
 
 class SearchResultFragment : Fragment() {
@@ -29,8 +33,7 @@ class SearchResultFragment : Fragment() {
 
     private val appContext = requireContext().applicationContext
     private var profileId: String = FirebaseAuth.getInstance().currentUser!!.uid
-    private var resultList= arrayListOf<String>()
-
+    private var resultList = arrayListOf<String>()
 
 
     override fun onCreateView(
@@ -61,7 +64,7 @@ class SearchResultFragment : Fragment() {
         }
     }
 
-    private fun fetchFilterFirebase(){
+    private fun fetchFilterFirebase() {
         lateinit var database: DatabaseReference
         lateinit var queryFirst: Query
         lateinit var querySecond: Query
@@ -79,56 +82,93 @@ class SearchResultFragment : Fragment() {
 
 
 
-        if(filterSeries){
+        if (filterSeries) {
             database = FirebaseDatabase.getInstance().getReference("Series")
-        }
-        else if(filterNovel){
+        } else if (filterNovel) {
             database = FirebaseDatabase.getInstance().getReference("Novel")
-        }
-        else if(filterEssay){
+        } else if (filterEssay) {
             database = FirebaseDatabase.getInstance().getReference("Essay")
-        }
-        else if(filterNSFW){
+        } else if (filterNSFW) {
             database = FirebaseDatabase.getInstance().getReference("NSFW")
         }
 
-        if(filterPopularity){
-            queryFirst = database.orderByChild("Popularity").equalTo("Popularity")
-        }
-        else if(filterRecent){
-            queryFirst = database.orderByChild("Recent").equalTo("Recent")
-        }
-        else{
+        if (filterPopularity) {
+            queryFirst = database.orderByChild("Stars")
+        } else if (filterRecent) {
+            queryFirst = database.orderByChild("releaseDateTime")
+        } else {
             queryFirst = database
         }
 
-        if(filterOngoing){
+        if (filterOngoing) {
             querySecond = queryFirst.orderByChild("Ongoing").equalTo("Ongoing")
-        }
-        else if(filterFinished){
+        } else if (filterFinished) {
             querySecond = queryFirst.orderByChild("Finished").equalTo("Finished")
-        }
-        else{
+        } else {
             querySecond = queryFirst
         }
 
 
 
-        querySecond.addValueEventListener(object : ValueEventListener{
+        querySecond.addValueEventListener(object : ValueEventListener {
+            @RequiresApi(Build.VERSION_CODES.N)
             override fun onDataChange(snapshot: DataSnapshot) {
                 resultList.clear()
-                if (snapshot.exists()) {
-                    for (element in snapshot.children) {
-                        val result = element.getValue(String::class.java)
-                        if (result != null) {
-                            resultList.add(result)
+
+                if (filterPopularity) {
+                    if (snapshot.exists()) {
+                        for (element in snapshot.children) {
+                            val result = element.getValue(String::class.java)
+                            if (result != null) {
+                                if (result.toInt() > 3) {
+                                    resultList.add(result)
+                                }
+                            }
                         }
                     }
-
                     adapter = SearchResultNavAdapter(resultList)
                     binding.searchResultRV.adapter = adapter
                 }
+                else if(filterRecent){
+                    if (snapshot.exists()) {
+                        for (element in snapshot.children) {
+                            val result = element.getValue(String::class.java)
+                            if (result != null) {
+                                val inputFormat = SimpleDateFormat("yyyy-MM-dd HH:mm")
+                                val resultDate = inputFormat.parse(result)
+
+                                val today = Date()
+                                val calendar = Calendar.getInstance()
+                                calendar.time = today
+
+                                calendar.add(Calendar.DATE, 3)
+                                val datePlusThreeDays = calendar.time
+
+                                if (resultDate.before(datePlusThreeDays)){
+                                    resultList.add(result)
+                                }
+                            }
+                        }
+                    }
+                    adapter = SearchResultNavAdapter(resultList)
+                    binding.searchResultRV.adapter = adapter
+                }
+
+                else{
+                    if (snapshot.exists()) {
+                        for (element in snapshot.children) {
+                            val result = element.getValue(String::class.java)
+                            if (result != null) {
+                                resultList.add(result)
+                            }
+                        }
+
+                        adapter = SearchResultNavAdapter(resultList)
+                        binding.searchResultRV.adapter = adapter
+                    }
+                }
             }
+
 
             override fun onCancelled(databaseError: DatabaseError) {
 
